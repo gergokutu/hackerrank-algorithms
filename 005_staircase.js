1a8a94a7be6f3389cb90badb1f2b354996bf985d
gergokutu <gergo.kutu@gmail.com>
2019-11-24 18:57:38 +0100

    Staircase problem with for loop

--- 005_staircase.js
+++ 005_staircase.js
@@ -2,15 +2,24 @@ const n = 6
 
 function staircase(n) {
   let stair, hash, space
-  let i = 0
 
+  // with while loop
+  let i = 0
   while (i < n) {
-    space = n - 1 - i;
-    hash = i + 1;
-    stair = ' '.repeat(space) + '#'.repeat(hash);
-    console.log(stair);
+    space = n - 1 - i
+    hash = i + 1
+    stair = ' '.repeat(space) + '#'.repeat(hash)
+    console.log(stair)
     i++
   }
+
+  // with for loop
+  // for (let i = 0; i < n; i++) {
+  //   space = n - 1 - i
+  //   hash = i + 1
+  //   stair = ' '.repeat(space) + '#'.repeat(hash)
+  //   console.log(stair)
+  // }
 }
 
 staircase(n)

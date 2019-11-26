f0af28cae43b633cd0180f39d25916224a7dfd4d
gergokutu <gergo.kutu@gmail.com>
2019-11-26 22:07:50 +0100

    Time conversion from 12 to 24

---
+++ 008_timeConversion.js
@@ -0,0 +1,31 @@
+const s = "12:40:22AM" // expected output: 19:05:45
+// get rid of am/pm
+// AM >> same
+// PM >> +12
+
+function timeConversion(s) {
+  const arr = s.slice(0, 8).split(':')
+
+  // ver1
+  // if (s.indexOf("PM") > -1) {
+  //   if (arr[0] === "12") { arr[0] = 12} 
+  //   else { arr[0] = Number(arr[0]) + 12 } // Number >> convert variables to number
+  // } 
+  // else {
+  //   if (arr[0] === "12") { arr[0] = 00 } // hackerrank >> compilation error, use "00"
+  // }
+
+  // ver2 >> with ternary operator
+  // arr[0] = s.indexOf("PM") > -1
+  //   ? arr[0] === "12" ? arr[0] = 12 : arr[0] = Number(arr[0]) + 12
+  //   : arr[0] === "12" ? arr[0] = 00 : arr[0]
+  
+  // ver3 >> refactor ver2
+  arr[0] = s.indexOf("PM") > -1
+    ? arr[0] === "12" ? 12 : Number(arr[0]) + 12
+    : arr[0] === "12" ? 00 : arr[0]
+
+  return arr.join(':')
+}
+
+console.log(timeConversion(s))

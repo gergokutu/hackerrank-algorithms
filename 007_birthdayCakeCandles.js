5bc4f60834ae5d26d7bef7bd02526df95e1affd0
gergokutu <gergo.kutu@gmail.com>
2019-11-26 17:50:02 +0100

    BirthdayCakeCandles 2 more solutions

--- 007_birthdayCakeCandles.js
+++ 007_birthdayCakeCandles.js
@@ -2,8 +2,24 @@ const ar = [3, 2, 1, 3]
 
 // Complete the birthdayCakeCandles function below.
 function birthdayCakeCandles(ar) {
+  // ...ar >> because it's an array and you have to spread
+  const max = Math.max(...ar)
 
-dvfvxvxf
+  // long version
+  // const result = ar.filter(highestNumber => {
+  //   if (highestNumber === max) {
+  //     return highestNumber
+  //   }})
+  // return result.length
+
+  // long version >> right synthax
+  // const result = ar.filter(highestNumber => {
+  // return (highestNumber === max)
+  // })
+  // return result.length
+
+  // short version
+  return ar.filter(highestNumber => highestNumber === max).length
 }
 
-birthdayCakeCandles(ar)
+console.log(birthdayCakeCandles(ar))

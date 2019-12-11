d75dc25d7c964ef0356364abff546e405a77b9c1
gergokutu <gergo.kutu@gmail.com>
2019-12-11 10:29:00 +0100

    Amend comments in 008_timeConversion.js

--- 008_timeConversion.js
+++ 008_timeConversion.js
@@ -1,9 +1,10 @@
 const s = "07:05:45PM" // expected output: 19:05:45
-// get rid of am/pm
-// AM >> same
-// PM >> +12
 
 function timeConversion(s) {
+  // get rid of am/pm (slice)
+  // create an array (with split)
+  // AM >> same
+  // PM >> +12
   const arr = s.slice(0, 8).split(':')
 
   // ver1
@@ -19,7 +20,7 @@ function timeConversion(s) {
   // arr[0] = s.indexOf("PM") > -1
   //   ? arr[0] === "12" ? arr[0] = 12 : arr[0] = Number(arr[0]) + 12
   //   : arr[0] === "12" ? arr[0] = 00 : arr[0]
-  
+
   // ver3 >> refactor ver2
   arr[0] = s.indexOf("PM") > -1
     ? arr[0] === "12" ? 12 : Number(arr[0]) + 12

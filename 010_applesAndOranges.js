0a30e5df3129601d449091578f79f41339872ae8
gergokutu <gergo.kutu@gmail.com>
2019-12-14 22:05:14 +0100

    Define apple and orange counter function

---
+++ 010_applesAndOranges.js
@@ -0,0 +1,24 @@
+const s = 7
+const t = 11
+const a = 5
+const b = 15
+const apples = [-2, 2, 1]
+const oranges = [5, -6]
+
+function countApplesAndOranges(s, t, a, b, apples, oranges) {
+  let appleCount = 0
+  let orangeCount = 0
+  
+  apples.map(apple => {
+    if (a + apple >= s && a + apple <= t) appleCount++
+  })
+
+  oranges.map(orange => {
+    if (b + orange >= s && b + orange <= t) orangeCount++
+  })
+
+  console.log('Number of apples:', appleCount)
+  console.log('number of oranges:', orangeCount)
+}
+
+countApplesAndOranges(s, t, a, b, apples, oranges)

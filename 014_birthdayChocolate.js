247865579d735a33cdc783dcdfe58906eba19901
gergokutu <gergo.kutu@gmail.com>
2019-12-16 14:42:02 +0100

    Setup 014_birthdayChocolate.js

---
+++ 014_birthdayChocolate.js
@@ -0,0 +1,17 @@
+// Algorithm should solve >> How many possible ways for s[0] + s[1] + ... + s[m] = d?
+// contiguous numbers counts only
+const s = [1, 2, 1, 3, 2]
+const d = 3
+const m = 2
+
+function birthday(s, d, m) {
+  let numberOfSolutions = 0
+
+  for (let i = 0; i < s.length -1; i++) {
+    
+  }
+
+  return numberOfSolutions
+}
+
+console.log(birthday(s, d, m))

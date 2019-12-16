1a9d85f32166a18621a20832feeadad1db911a6e
gergokutu <gergo.kutu@gmail.com>
2019-12-16 13:59:22 +0100

    Refactor 013_breakingRecords.js

--- 013_breakingRecords.js
+++ 013_breakingRecords.js
@@ -1,7 +1,36 @@
 const scores = [10, 5, 20, 20, 4, 5, 2, 25, 1]
 
 function breakingRecords(scores) {
-  console.log(scores)
+  // ver2 >> only if
+  let min = scores[0]
+  let max = scores[0]
+  let result = [0, 0]
+  
+  scores.map(score => {
+    if (score > max) { max = score; result[0]++ }
+    if (score < min) { min = score; result[1]++ }
+  })
+
+  return result
+
+  // ver1 >> if, else if, else
+  // let min = scores[0]
+  // let max = scores[0]
+  // let result = [0, 0]
+  
+  // scores.map(score => {
+  //   if (score > max) {
+  //     max = score
+  //     result[0]++
+  //   } else if (score < min) {
+  //     min = score
+  //     result[1]++
+  //   } else {
+  //     result
+  //   }
+  // })
+
+  // return result
 }
 
-breakingRecords(scores)
+console.log(breakingRecords(scores))

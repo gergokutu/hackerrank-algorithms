040ab62ce1d58360a43e774006ecc576d1038de1
gergokutu <gergo.kutu@gmail.com>
2019-12-19 11:44:10 +0100

    Refactor dayofProgrammer - ver2

--- 017_dayOfProgrammer.js
+++ 017_dayOfProgrammer.js
@@ -1,25 +1,39 @@
 const year = 2017
 
 function dayOfProgrammer(year) {
-  if (year < 1918 && year % 4 === 0) {
-    return `12.09.${year}`
-  }
-
-  if (year < 1918 && year % 4 !== 0) {
-    return `13.09.${year}`
-  }
-
-  if (year > 1918 && (year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0))) {
-    return `12.09.${year}`
-  }
-
-  if (year > 1918 && !(year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0))) {
-    return `13.09.${year}`
-  }
-
+  // ver2 >> refactored
   if (year === 1918) {
     return '26.09.1918'
+  } else {
+    if (year < 1918 && year % 4 === 0 || year > 1918 && (year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0))) {
+      return `12.09.${year}`
+    }
+  
+    if (year < 1918 && year % 4 !== 0 || year > 1918 && !(year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0))) {
+      return `13.09.${year}`
+    }
   }
+
+  // ver1 >> with if statements
+  // if (year < 1918 && year % 4 === 0) {
+  //   return `12.09.${year}`
+  // }
+
+  // if (year < 1918 && year % 4 !== 0) {
+  //   return `13.09.${year}`
+  // }
+
+  // if (year > 1918 && (year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0))) {
+  //   return `12.09.${year}`
+  // }
+
+  // if (year > 1918 && !(year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0))) {
+  //   return `13.09.${year}`
+  // }
+
+  // if (year === 1918) {
+  //   return '26.09.1918'
+  // }
 }
 
 console.log(dayOfProgrammer(year))

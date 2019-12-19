41b8b40482fe1d84d5a5456afe1f01053df1fa9f
gergokutu <gergo.kutu@gmail.com>
2019-12-19 11:21:23 +0100

    DayofProgrammer solved with if statements - ver1

--- 017_dayOfProgrammer.js
+++ 017_dayOfProgrammer.js
@@ -1,7 +1,25 @@
 const year = 2017
 
 function dayOfProgrammer(year) {
+  if (year < 1918 && year % 4 === 0) {
+    return `12.09.${year}`
+  }
 
+  if (year < 1918 && year % 4 !== 0) {
+    return `13.09.${year}`
+  }
+
+  if (year > 1918 && (year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0))) {
+    return `12.09.${year}`
+  }
+
+  if (year > 1918 && !(year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0))) {
+    return `13.09.${year}`
+  }
+
+  if (year === 1918) {
+    return '26.09.1918'
+  }
 }
 
 console.log(dayOfProgrammer(year))

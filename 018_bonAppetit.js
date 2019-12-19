bc1ce78df5dc037e6d51cf87cf528f48334d03b1
gergokutu <gergo.kutu@gmail.com>
2019-12-19 16:49:27 +0100

    Refactor 018_bonAppetit.js

--- 018_bonAppetit.js
+++ 018_bonAppetit.js
@@ -3,15 +3,20 @@ const k = 1
 const b = 12
 
 function bonAppetit(bill, k, b) {
-  const total = bill.reduce((accumulator, currentValue) => accumulator + currentValue)
-  const charged = (total - bill[k]) / 2
+  // ver2 >> refactored
+  const charged = (bill.reduce((accumulator, currentValue) => accumulator + currentValue) - bill[k]) / 2
+  b === charged ? console.log('Bon Appetit') : console.log(b - charged)
 
-  if (b === charged) {
-   console.log('Bon Appetit')
-  }
-  else {
-   console.log(b - charged)
-  }
+  // ver1
+  // const total = bill.reduce((accumulator, currentValue) => accumulator + currentValue)
+  // const charged = (total - bill[k]) / 2
+
+  // if (b === charged) {
+  //  console.log('Bon Appetit')
+  // }
+  // else {
+  //  console.log(b - charged)
+  // }
 }
 
 bonAppetit(bill, k, b)

1e7cc14f8d8fcaa6179eb6c5d37674544111f353
gergokutu <gergo.kutu@gmail.com>
2019-12-19 11:53:35 +0100

    Refactor dayofProgrammer with else if - ver3

--- 017_dayOfProgrammer.js
+++ 017_dayOfProgrammer.js
@@ -1,18 +1,27 @@
 const year = 2017
 
 function dayOfProgrammer(year) {
-  // ver2 >> refactored
+  // ver3 >> refactored again with if, else if, else
   if (year === 1918) {
     return '26.09.1918'
-  } else {
-    if (year < 1918 && year % 4 === 0 || year > 1918 && (year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0))) {
+  } else if (year < 1918 && year % 4 === 0 || year > 1918 && (year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0))) {
       return `12.09.${year}`
-    }
-  
-    if (year < 1918 && year % 4 !== 0 || year > 1918 && !(year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0))) {
-      return `13.09.${year}`
-    }
+  } else {
+    return `13.09.${year}`
   }
+
+  // ver2 >> refactored
+  // if (year === 1918) {
+  //   return '26.09.1918'
+  // } else {
+  //   if (year < 1918 && year % 4 === 0 || year > 1918 && (year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0))) {
+  //     return `12.09.${year}`
+  //   }
+  
+  //   if (year < 1918 && year % 4 !== 0 || year > 1918 && !(year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0))) {
+  //     return `13.09.${year}`
+  //   }
+  // }
 
   // ver1 >> with if statements
   // if (year < 1918 && year % 4 === 0) {

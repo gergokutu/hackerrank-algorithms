45790eefca2054dc19daefa906136571a6ec3538
gergokutu <gergo.kutu@gmail.com>
2020-02-18 08:47:46 +0100

    020_drawingBook ver1

---
+++ 020_drawingBook.js
@@ -0,0 +1,19 @@
+function pageCount(numberOfPages, pageToTurn) {
+  let turn = 0
+
+  if (numberOfPages / 2 >= pageToTurn) {
+    for (let i = 1; i < pageToTurn; i += 2) turn++
+  } else {
+    if (numberOfPages % 2 === 0) {
+      for (let i = numberOfPages; i > pageToTurn; i -= 2) turn++
+    } else {
+      for (let i = numberOfPages - 1; i > pageToTurn; i -= 2) turn++
+    }
+  }
+
+  return turn
+}
+
+const n = 11
+const p = 10
+console.log(pageCount(n, p))

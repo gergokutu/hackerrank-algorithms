9d9e9c5870789e69aa5854be149e2689a628efe0
gergokutu <gergo.kutu@gmail.com>
2020-02-18 09:04:52 +0100

    Refactor ver1

--- 020_drawingBook.js
+++ 020_drawingBook.js
@@ -1,18 +1,35 @@
+// ver1
+// function pageCount(numberOfPages, pageToTurn) {
+//   let turn = 0
+
+//   if (numberOfPages / 2 >= pageToTurn) {
+//     for (let i = 1; i < pageToTurn; i += 2) turn++
+//   } else {
+//     if (numberOfPages % 2 === 0) {
+//       for (let i = numberOfPages; i > pageToTurn; i -= 2) turn++
+//     } else {
+//       for (let i = numberOfPages - 1; i > pageToTurn; i -= 2) turn++
+//     }
+//   }
+
+//   return turn
+// }
+
+// ver2
 function pageCount(numberOfPages, pageToTurn) {
   let turn = 0
 
   if (numberOfPages / 2 >= pageToTurn) {
     for (let i = 1; i < pageToTurn; i += 2) turn++
+  } else if (numberOfPages / 2 < pageToTurn && numberOfPages % 2 === 0) {
+    for (let i = numberOfPages; i > pageToTurn; i -= 2) turn++
   } else {
-    if (numberOfPages % 2 === 0) {
-      for (let i = numberOfPages; i > pageToTurn; i -= 2) turn++
-    } else {
-      for (let i = numberOfPages - 1; i > pageToTurn; i -= 2) turn++
-    }
+    for (let i = numberOfPages - 1; i > pageToTurn; i -= 2) turn++
   }
 
   return turn
 }
+
 
 const n = 11
 const p = 10

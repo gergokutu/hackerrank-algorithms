3753966cfe73ea021a99aa52fbd0c07b1d03b099
gergokutu <gergo.kutu@gmail.com>
2020-02-18 09:17:23 +0100

    Refactor short version

--- 020_drawingBook.js
+++ 020_drawingBook.js
@@ -31,15 +31,23 @@
 // }
 
 // ver3
-function pageCount(numberOfPages, pageToTurn) {
-  const frontFlip = Math.floor(p/2)
-  // total turns - turns from the beginning
-  const backFlip = Math.floor((n/2)-frontFlip)
+// function pageCount(numberOfPages, pageToTurn) {
+//   const frontFlip = Math.floor(p/2)
+//   // total turns - turns from the beginning
+//   const backFlip = Math.floor((n/2)-frontFlip)
   
-  const result = Math.min(frontFlip, backFlip)
-  return result
+//   const result = Math.min(frontFlip, backFlip)
+//   return result
+// }
+
+// ver4
+function pageCount(numberOfPages, pageToTurn) {
+  const frontFlip = Math.floor(pageToTurn/2)
+  const backFlip = Math.floor((numberOfPages/2)-frontFlip)
+  
+  return frontFlip < backFlip ? frontFlip : backFlip
 }
 
-const n = 11
-const p = 4
+const n = 10
+const p = 9
 console.log(pageCount(n, p))

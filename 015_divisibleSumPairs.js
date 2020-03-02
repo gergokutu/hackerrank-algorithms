2a2c821f93e28bf268654d55368b5d826eb0ff54
gergokutu <gergo.kutu@gmail.com>
2020-03-02 14:19:08 +0100

    Comment

--- 015_divisibleSumPairs.js
+++ 015_divisibleSumPairs.js
@@ -1,11 +1,11 @@
-const n = 6
+const n = 6 // length of the array..
 const k = 3
 const ar = [1, 3, 2, 6, 1, 2]
 
 function divisibleSumPairs(n, k, ar) {
   let counter = 0
 
-  for (let i = 0; i < n -1; i++) {
+  for (let i = 0; i < n - 1; i++) {
     for (let j = 1; j < n; j++) {
       if (i < j && (ar[i] + ar[j]) % k === 0) counter++
     }
